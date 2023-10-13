390e0db3a14abb60d1c37f06505168bba05a5111
gouider heidi <gouider.heidi@gmail.com>
2023-10-13 10:15:45 +0200

    .

---
+++ src/models/explication.php
@@ -0,0 +1,2 @@
+<!-- Le dossier "Models" peut contenir les fichiers PHP qui représentent les modèles de données de l'application.
+Cela inclut généralement les interactions avec la base de données, les requêtes SQL, etc. -->
